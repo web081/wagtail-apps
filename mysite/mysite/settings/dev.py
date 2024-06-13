6c64e4ebf161b8f278e832518e47e2c33a3297f8
Kole <koladevops@outlook.com>
2024-06-13 09:37:01 +0100

    Add CI/CD pipeline

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-7s@-ha957h1$(m&!)+z=p^%*5)53*5wwq!s2i(n0e-z8kn$g8%"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
